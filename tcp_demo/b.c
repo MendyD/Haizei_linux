913ca35deaf124d6c3d7b582ed92c55153c4c4c2
MendyD <dolai_c@163.com>
2019-04-09 10:45:26 +0800

    pi2

---
+++ tcp_demo/b.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: a.c
+	> Author: Mendy
+	> Mail: dolai_c@163.com
+	> Course: 
+	> Created Time: 六  2/23 16:33:47 2019
+ ************************************************************************/
+ #include "common.h"
+
+int main(int argc, char *argv[]){
+    char *host = argv[1];
+    struct sockaddr_in sock_addr;
+    sock_addr.sin_addr.s_addr = inet_addr(host);
+    char buff[20] = {0};
+    for(int i = 0; i < 3; i++){
+        int a = sock_addr.sin_addr.s_addr & 255;
+        sock_addr.sin_addr.s_addr = sock_addr.sin_addr.s_addr >> 8;
+        sprintf(buff , "%d.",a);
+        printf("%s", buff);
+    }
+    int a = sock_addr.sin_addr.s_addr & 255;
+    sock_addr.sin_addr.s_addr = sock_addr.sin_addr.s_addr >> 8;
+    sprintf(buff, "%d", a);
+    printf("%s\n", buff);
+    return 0;
+ }
